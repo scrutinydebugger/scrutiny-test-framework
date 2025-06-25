35108cacc260c6e960e4f09c5bdf7828542d0f72
Pier-Yves Lessard <py.lessard@gmail.com>
2025-06-24 20:14:33 -0400

    codebanner and format script

---
+++ scripts/format_code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+APP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+cd "$APP_ROOT"
+
+format_dir()
+{
+    find $1 -iname '*.hpp' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.h'  | xargs clang-format -i
+}
+
+format_dir ./scrutinytest
+format_dir ./tests
